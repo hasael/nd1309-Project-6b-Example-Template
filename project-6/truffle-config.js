6880e769b92a5d66a522bc01d648767a4eab36a1
Elman Hasa <ElmanH@motabilityoperations.co.uk>
2021-11-26 17:06:26 +0000

    Add contract deployed in Rinkeby

--- project-6/truffle-config.js
+++ project-6/truffle-config.js
@@ -1,3 +1,7 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "<hidden>";
+const mnemonic = "<hidden>";
+
 module.exports = {
   networks: {
     development: {
@@ -5,7 +9,13 @@ module.exports = {
       port: 8545,
       network_id: "*", // Match any network id
       websockets: true
-    }
+    },
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${infuraKey}`, 1),
+      network_id: 4,       // rinkeby's id
+      gas: 4500000,        // rinkeby has a lower block limit than mainnet
+      gasPrice: 10000000000
+    },
   },
   compilers: {
     solc: {
